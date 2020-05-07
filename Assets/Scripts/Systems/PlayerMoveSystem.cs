d9e8080f1d3638e8ba3c94cc49c388e005816bc7
Rod Ellison <rod.r.ellison@gmail.com>
2020-05-07 13:29:39 -0400

    adding some Z rotation to the XWing when player is moving left or right.. just looks better

--- Assets/Scripts/Systems/PlayerMoveSystem.cs
+++ Assets/Scripts/Systems/PlayerMoveSystem.cs
@@ -9,6 +9,8 @@ namespace Systems
     {
         public FloatVariable PlayerMoveSpeed;
         public FloatVariable maxPlayerHorizontalDistance;
+        Quaternion rotationMin = Quaternion.Euler(new Vector3(0f, 0f, -20f));
+        Quaternion rotationMax = Quaternion.Euler(new Vector3(0f, 0f, 20f));
 
         // Start is called before the first frame update
         public InputActionControls playerControls;
@@ -27,23 +29,41 @@ namespace Systems
         void Update()
         {
             var deltaX = inputValue.x * PlayerMoveSpeed.Value * Time.deltaTime;
-            if (inputValue.x != 0 && 
-                (transform.position.x + deltaX < maxPlayerHorizontalDistance.Value && 
+            float currentYPos = transform.position.y;
+            Quaternion currentRotation = transform.rotation;
+
+            if (inputValue.x != 0 &&
+                (transform.position.x + deltaX < maxPlayerHorizontalDistance.Value &&
                  transform.position.x + deltaX > -maxPlayerHorizontalDistance.Value))
             {
                 transform.Translate(deltaX, 0f, 0f);
+
+                transform.rotation = (inputValue.x > 0)
+                    ? Quaternion.Lerp(currentRotation, rotationMax, Time.deltaTime * 5f)
+                    : Quaternion.Lerp(currentRotation, rotationMin, Time.deltaTime * 5f);
             }
-  
+            else
+            {
+                transform.rotation = Quaternion.Slerp(currentRotation, Quaternion.identity, Time.deltaTime * 5f);
+            }
+
             if (inputValue.z != 0)
             {
                 var deltaPosition = inputValue.z * PlayerMoveSpeed.Value * 0.5f * Time.deltaTime;
                 var newTransformPosition = transform.position.y + deltaPosition;
-        //        Debug.Log(newTransformPosition);
+                //        Debug.Log(newTransformPosition);
                 //This is to make sure we keep the player in Vertical zone where we need..
                 if (newTransformPosition > 20f || newTransformPosition < 4f)
                     return;
 
                 transform.Translate(0f, deltaPosition, 0f);
+            }
+            else
+            {
+                //This ensures the XWing doesnt float up when doing things like Euler rotations..
+                Vector3 tempPos = transform.position;
+                tempPos.y = currentYPos;
+                transform.position = tempPos;
             }
         }
 
@@ -53,14 +73,12 @@ namespace Systems
             inputValue.x = input2D.x;
             inputValue.z = input2D.y;
 
-      //      Debug.Log(" Move called");
+            //      Debug.Log(" Move called");
         }
 
         public void OnShoot(InputAction.CallbackContext context)
         {
-          //Nothing to do for this in this script..Shooting is handled in the PlayerShootSystem script
-
+            //Nothing to do for this in this script..Shooting is handled in the PlayerShootSystem script
         }
     }
 }
-
